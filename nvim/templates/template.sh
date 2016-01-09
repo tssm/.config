f58e3f4ba207fefdf3b4b3a62423666f4cf05224
Tae Sandoval <tssm@users.noreply.github.com>
2016-01-09 16:08:34 -0300

    Add OWN_DIR value

--- nvim/templates/template.sh
+++ nvim/templates/template.sh
@@ -3,4 +3,12 @@
 set -o errexit
 set -o nounset
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+readonly OWN_DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
 %HERE%
