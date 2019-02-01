e05ce833daf87a3fc28785f0c576861139aac15d
Tae Sandoval <tssm@users.noreply.github.com>
2019-01-31 21:35:24 -0300

    Replace Bash with Fish

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+set vimr /Applications/VimR.app/Contents/Resources/vimr
+if [ -e $vimr ]
+	set --export EDITOR $vimr
+else
+	set --export EDITOR nvim
+end
+
+# XDG
+set --export XDG_CACHE_HOME $HOME/.cache
+set --export XDG_CONFIG_HOME $HOME/.config
+set --export XDG_DATA_HOME $HOME/.local/share
+
+# Nix
+set --export NIX_PATH nixpkgs=$HOME/.nix-defexpr/channels
+set --export NIX_PROFILES /nix/var/nix/profiles/default $HOME/.nix-profile
+set --export SSL_CERT_FILE $HOME/.nix-profile/etc/ssl/certs/ca-bundle.crt
+
+# NPM
+set --export NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/config
+
+# Python
+set PYTHON_VERSION (find $NIX_LINK/lib -regex '.*/python[0-9]*\.[0-9]*' \
+	| sort --version-sort \
+	| tail -n 2)
+set --export PYTHONPATH $NIX_LINK/lib/(basename $PYTHON_VERSION)/site-packages
+
+for profile in $NIX_PROFILES
+	set PATH $profile/bin $PATH
+end
+set PATH $HOME/.local/bin $PATH
+set PATH $HOME/.luarocks/bin $PATH
+set PATH ./node_modules/.bin $XDG_DATA_HOME/node_modules/bin $PATH
+set --export PATH $PATH
---
+++ fish/functions/vb.fish
@@ -0,0 +1,3 @@
+function vb
+	VBoxManage $argv
+end
