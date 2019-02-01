74cda63589bf3a35c647e4ade25c75609c99a134
Tae Sandoval <tssm@users.noreply.github.com>
2019-02-01 18:18:57 -0300

    Fish: Show Pijul's branch on prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,15 @@
 function fish_prompt
-	set git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+	set git_branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+	set pijul_branch (pijul status 2> /dev/null \
+		| head -n 1 \
+		| grep --extended-regexp --only-matching '[^ ]+$')
+			echo -n "on git's "
+			echo -n $git_branch " "
+		else if [ $pijul_branch ]
+			set_color $fish_color_normal
+			echo -n "on pijul's "
+			set_color magenta
+			echo -n $pijul_branch " "
 	set home [ (pwd) = $HOME ]
 	set remote [ -n "$SSH_CLIENT" -o -n "$SSH_CONNECTION" -o -n "$SSH_TTY" ]
 	set sudo [ -n "$SUDO_USER" ]
@@ -37,7 +47,8 @@ function fish_prompt
 			echo -n " "
 		end
 	end
-	if test -n "$git_branch" || not $home
+	if test -n "$git_branch" || test -n "$pijul_branch" || not $home
+		# Add a new line if the prompt has directory data
 		echo
 	end
 	set_color cyan
