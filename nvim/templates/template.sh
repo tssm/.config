95ccae4dcc2c7e2489ecfe11d5e974c5b4a71cd5
Tae Sandoval <tssm@users.noreply.github.com>
2019-04-03 13:55:53 -0300

    Neovim: Remove Bash boilerplate from template

--- nvim/templates/template.sh
+++ nvim/templates/template.sh
@@ -1,14 +1,3 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do
-  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-done
-readonly OWN_DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+#!/usr/bin/env sh
 
 %HERE%
