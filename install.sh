ea5fe8026320c08e856537f6edb09cb8dbc9437d
Tae Sandoval <tssm@users.noreply.github.com>
2020-08-31 21:56:15 +0200

    Neovim: Manage plugins with Nix

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ mkdir -p ~/.cache/psql_history
 
 # Neovim
 
-git clone git@github.com:tssm/neovim-automaton \
-	~/.local/share/nvim/site/pack/automaton/start/neovim-automaton
-
 NVIM_SPELL_DEST=~/.local/share/nvim/site/spell/
 NVIM_SPELL_EXTENSION=.utf-8.spl
 NVIM_SPELL_SOURCE=http://ftp.vim.org/vim/runtime/spell/
