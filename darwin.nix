ab9fda78b1e32f08620042e4fa4100682bf16e35
Tae Sandoval <tssm@users.noreply.github.com>
2021-02-02 22:25:00 +0100

    Format Nix files with nixpkgs-fmt
    nixpkgs-fmt is the formatter used by rnix-lsp, the only Nix language
    server for the moment

--- darwin.nix
+++ darwin.nix
@@ -1,36 +1,36 @@
 { pkgs, ... }: {
-	environment.darwinConfig = "$HOME/.config/darwin.nix";
-	environment.systemPackages = [
-		pkgs.bat
-		pkgs.cabal-install
-		pkgs.darwin.trash
-		pkgs.dhall
-		pkgs.dhall-json
-		pkgs.dhall-lsp-server
-		pkgs.diffr
-		pkgs.fd
-		pkgs.fish
-		pkgs.fzf
-		pkgs.ghc
-		pkgs.haskellPackages.haskell-language-server
-		pkgs.neovim
-		pkgs.neovim-remote
-		pkgs.pijul
-		pkgs.ripgrep
-		pkgs.rls
-		pkgs.rnix-lsp
-		pkgs.tree
-	];
+  environment.darwinConfig = "$HOME/.config/darwin.nix";
+  environment.systemPackages = [
+    pkgs.bat
+    pkgs.cabal-install
+    pkgs.darwin.trash
+    pkgs.dhall
+    pkgs.dhall-json
+    pkgs.dhall-lsp-server
+    pkgs.diffr
+    pkgs.fd
+    pkgs.fish
+    pkgs.fzf
+    pkgs.ghc
+    pkgs.haskellPackages.haskell-language-server
+    pkgs.neovim
+    pkgs.neovim-remote
+    pkgs.pijul
+    pkgs.ripgrep
+    pkgs.rls
+    pkgs.rnix-lsp
+    pkgs.tree
+  ];
 
-	fonts.enableFontDir = true;
-	fonts.fonts = [ pkgs.jetbrains-mono ];
+  fonts.enableFontDir = true;
+  fonts.fonts = [ pkgs.jetbrains-mono ];
 
-	nix.buildCores = 8; # $ sysctl -n hw.ncpu
-	nix.maxJobs = 8; # $ sysctl -n hw.ncpu
-	nix.trustedUsers = [ "@admin" ];
+  nix.buildCores = 8; # $ sysctl -n hw.ncpu
+  nix.maxJobs = 8; # $ sysctl -n hw.ncpu
+  nix.trustedUsers = [ "@admin" ];
 
-	nixpkgs.config = { allowUnfree = true; };
-	nixpkgs.overlays = [ (import nixpkgs/overlays/neovim.nix) ];
+  nixpkgs.config = { allowUnfree = true; };
+  nixpkgs.overlays = [ (import nixpkgs/overlays/neovim.nix) ];
 
-	services.nix-daemon.enable = true;
+  services.nix-daemon.enable = true;
 }
