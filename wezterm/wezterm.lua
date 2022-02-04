31c5a19c55d15dd02d2129e27b1138a410ff9f97
Tae Selene Sandoval Murgan <tssm@users.noreply.github.com>
2022-02-04 16:35:07 +0100

    WezTerm: Add configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+
+return {
+	default_prog = {'/run/current-system/sw/bin/fish', '--login', '--command', 'nvim'},
+	enable_tab_bar = false,
+	font = wezterm.font('JetBrainsMono Nerd Font Mono'),
+	harfbuzz_features = {'calt=0', 'clig=0', 'liga=0'},
+	keys = {
+		{key = 'f', mods = 'CMD|CTRL', action = 'ToggleFullScreen'},
+		{key = 'n', mods = 'CMD', action = 'SpawnWindow'},
+		{key = 'q', mods = 'CMD', action = 'DisableDefaultAssignment'},
+		{key = 's', mods = 'CMD', action = wezterm.action {SendString = ':wall\n'}},
+		{key = 'w', mods = 'CMD', action = wezterm.action {SendString = ':tabclose\n'}},
+		{key = 'w', mods = 'CMD|SHIFT', action = wezterm.action {SendString = ':qall\n'}},
+	},
+	native_macos_fullscreen_mode = true,
+	send_composed_key_when_left_alt_is_pressed = true,
+	send_composed_key_when_right_alt_is_pressed = true,
+}
