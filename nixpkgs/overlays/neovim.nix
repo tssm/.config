a835219f52882b221f1fcdd578431cc79b95dede
Tae Selene Sandoval Murgan <tssm@users.noreply.github.com>
2022-12-12 12:54:10 -0300

    Neovim: Install all tree-sitter grammars with Nix

--- nixpkgs/overlays/neovim.nix
+++ nixpkgs/overlays/neovim.nix
@@ -78,7 +78,7 @@ in
 
           # Tree-sitter
           (plug "nvim-treesitter" "nvim-treesitter" [ ])
-          (super.pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: super.pkgs.tree-sitter.allGrammars))
+          super.pkgs.vimPlugins.nvim-treesitter.withAllGrammars
           (plug "nvim-treesitter" "nvim-treesitter-refactor" [ ./tree-sitter-refactor.patch ])
           (plug "nvim-treesitter" "nvim-treesitter-textobjects" [ ])
           (plug "windwp" "nvim-autopairs" [ ])
